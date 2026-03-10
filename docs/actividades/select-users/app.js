c780f394f7c20d71ff457db99e84008bdc96f417
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-03-09 20:37:47 -0600

    feat(select-users): add posts

--- docs/actividades/select-users/app.js
+++ docs/actividades/select-users/app.js
@@ -1,15 +1,37 @@
-const selectUsers = document.getElementById("selectUsers");
-const divDatos = document.getElementById("divDatos");
-const btnCargar = document.getElementById("btnCargar");
+    const selectUsers = document.getElementById("selectUsers");
+    const divPosts = document.getElementById("divPosts");
+    const btnCargar = document.getElementById("btnCargar");
 
-btnCargar.addEventListener("click", () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json())
-        .then(users => {
-            let optionUsers = "";
-            users.forEach(user => {
-                optionUsers += `<option value=${user.id}>${user.username}</option>`
+    btnCargar.addEventListener("click", () => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => {
+                let optUsers = "";
+                users.forEach(user => {
+                    optUsers += `<option value=${user.id}>${user.username}</option>`
+                });
+                selectUsers.innerHTML = optUsers;
             });
-            selectUsers.innerHTML = optionUsers;
-        });
-});
+    });
+
+    selectUsers.addEventListener("change", () => {
+        fetch(`https://jsonplaceholder.typicode.com/posts?userId=${selectUsers.value}`)
+            .then(response => response.json())
+            .then(posts => {
+                let divUserPosts = "";
+
+                posts.forEach(post => {
+                    divUserPosts += `
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${post.title}</h5>
+                            <p class="card-text">${post.body}</p>
+                            <button class="btn btn-primary" onclick="verComentarios(${post.id})">Ver comentarios</button>
+                    </div>
+                    
+                    <div id="post-${post.id}></div>`;
+                });
+
+                divPosts.innerHTML = divUserPosts;
+            });
+    });
