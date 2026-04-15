de3038d733e4cb81540cb33f54dcab98c533517c
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-14 23:42:44 -0600

    feat: add EventListener btnAgregar

--- docs/actividades/api-vehiculos/app.js
+++ docs/actividades/api-vehiculos/app.js
@@ -4,10 +4,16 @@ const btnBuscar = document.getElementById("btnBuscar");
 
 const lista = document.getElementById("lista");
 
-let placa = document.getElementById("placa").value;   
-let marca = document.getElementById("marca").value;  
-let modelo = document.getElementById("modelo").value;
+btnAgregar.addEventListener("click", async () => {
+    const placa = document.getElementById("placa").value;
+    const marca = document.getElementById("marca").value;
+    const modelo = document.getElementById("modelo").value;
 
-btnAgregar.addEventListener("click", () => {
-    fetch("")
-})
+    const res = await fetch("http://localhost:3000/vehiculos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ placa, marca, modelo })
+    });
+
+    console.log(res.json());
+});
